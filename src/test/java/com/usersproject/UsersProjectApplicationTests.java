504e5346281a669d0512a35c9f4013b4ae7adfc6
DESKTOP-C1LSTMR\USER <omerp@webtech-inv.co.il>
2021-11-21 10:33:20 +0200

    Initial commit

---
+++ src/test/java/com/usersproject/UsersProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usersproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
